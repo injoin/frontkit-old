ec11f680265bbe5d36edfdcd02ee67affbe1993a
Gustavo Henke <gustavo@injoin.com.br>
2013-06-11 08:09:43 -0300

    Small flag for telling whether mobile resolutions will have the affix too

--- scripts/affix.js
+++ scripts/affix.js
@@ -6,15 +6,24 @@
 
     $.frontkit( "affix", {
         top: 0,
+        active: false,
 
         options: {
             offset: 0,
-            position: 0
+            position: 0,
+            mobile: true,
+
+            // Event callbacks
+            activate: null
         },
 
         _initialize: function() {
             this.top = this.element.offset().top;
             this.element.addClass( "affix" );
+
+            if ( !this.options.mobile ) {
+                this.element.addClass( "affix-no-mobile" );
+            }
 
             $instances = $instances.add( this.element );
         },
@@ -28,6 +37,8 @@
         },
 
         _positionElement: function( scrollTop ) {
+            var wasActive = this.active;
+
             if ( this.element.is( ":hidden" ) ) {
                 return;
             }
@@ -35,8 +46,22 @@
             if ( scrollTop > ( this.top - this.options.offset ) ) {
                 this.element.css( "top", this.options.position );
                 this.element.removeClass( "affix-inactive" );
+
+                if ( !wasActive ) {
+                    this.active = true;
+                    this._trigger( "activate", {}, {
+                        active: true
+                    });
+                }
             } else {
                 this.element.addClass( "affix-inactive" );
+
+                if ( wasActive ) {
+                    this.active = false;
+                    this._trigger( "activate", {}, {
+                        active: false
+                    });
+                }
             }
         }
     });
