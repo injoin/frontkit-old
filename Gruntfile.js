719f06b0b243423286f31bcf30b5604d47c0583e
Gustavo Henke <gustavo@injoin.com.br>
2013-07-18 11:39:32 -0300

    Grunt tests with each supported jQuery versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,82 @@
 module.exports = exports = function( grunt ) {
     "use strict";
 
-    grunt.initConfig( exports.tasks );
+    var _ = grunt.util._;
+
+    function jqueryTestPage( file, version ) {
+        return "http://localhost:<%= connect.dev.options.port %>/tests/unit/" + file + ".html?jquery=" + version;
+    }
+
+    grunt.initConfig({
+        watch: {
+            less: {
+                files: [ "styles/*.less" ],
+                tasks: [ "default" ]
+            },
+            js: {
+                files: [ "scripts/*.js" ],
+                tasks: [ "concat" ]
+            }
+        },
+        jshint: {
+            dev: {
+                options: {
+                    jshintrc: ".jshintrc"
+                },
+                files: {
+                    src: [ "scripts/*.js" ]
+                }
+            }
+        },
+        qunit: {
+            dev: {
+                options: {
+                    urls: _.flatten([ "1.9.0", "1.10.0", "2.0.0" ].map(function( version ) {
+                        return [
+                            jqueryTestPage( "core", version ),
+                            jqueryTestPage( "affix", version )
+                        ];
+                    }))
+                }
+            }
+        },
+        jscs: {
+            dev: [ "scripts/*.js", "tests/unit/spec/**/*.js" ]
+        },
+        jquery: {
+            dev: {
+                output: "tests/unit/lib/jquery",
+                versions: [ "1.9.0", "1.10.0", "2.0.0" ]
+            }
+        },
+        connect: {
+            dev: {
+                options: {
+                    port: 5050
+                }
+            }
+        },
+        concat: {
+            dev: {
+                src: [ "scripts/core.js", "scripts/*.js" ],
+                dest: "dist/frontkit.js"
+            }
+        },
+        less: {
+            dev: {
+                files: {
+                    "dist/frontkit.css": "styles/frontkit.less"
+                }
+            }
+        }
+    });
 
     grunt.loadNpmTasks( "grunt-contrib-less" );
     grunt.loadNpmTasks( "grunt-contrib-watch" );
     grunt.loadNpmTasks( "grunt-contrib-concat" );
     grunt.loadNpmTasks( "grunt-contrib-qunit" );
     grunt.loadNpmTasks( "grunt-contrib-jshint" );
+    grunt.loadNpmTasks( "grunt-contrib-connect" );
     grunt.loadNpmTasks( "grunt-jscs-checker" );
     grunt.loadNpmTasks( "grunt-jquery-builder" );
 
@@ -16,59 +85,9 @@ module.exports = exports = function( grunt ) {
         "jshint:dev",
         "jscs:dev",
         "jquery:dev",
+        "connect:dev",
         "qunit:dev",
         "concat:dev"
     ]);
     grunt.registerTask( "default", [ "styles", "scripts" ] );
-};
-
-exports.tasks = {
-    watch: {
-        less: {
-            files: [ "styles/*.less" ],
-            tasks: [ "default" ]
-        },
-        js: {
-            files: [ "scripts/*.js" ],
-            tasks: [ "concat" ]
-        }
-    },
-    jshint: {
-        dev: {
-            options: {
-                jshintrc: ".jshintrc"
-            },
-            files: {
-                src: [ "scripts/*.js" ]
-            }
-        }
-    },
-    qunit: {
-        dev: [
-            "tests/unit/core.html",
-            "tests/unit/affix.html"
-        ]
-    },
-    jscs: {
-        dev: [ "scripts/*.js", "tests/unit/spec/**/*.js" ]
-    },
-    jquery: {
-        dev: {
-            output: "tests/unit/lib/jquery",
-            versions: [ "1.9.0", "1.10.0", "2.0.0" ]
-        }
-    },
-    concat: {
-        dev: {
-            src: [ "scripts/core.js", "scripts/*.js" ],
-            dest: "dist/frontkit.js"
-        }
-    },
-    less: {
-        dev: {
-            files: {
-                "dist/frontkit.css": "styles/frontkit.less"
-            }
-        }
-    }
 };
