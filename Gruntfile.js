64e11a779a15f538e49b739206c79bfd8b72a8f2
Gustavo Henke <gustavo@injoin.com.br>
2013-07-20 00:46:31 -0300

    Refactored Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,60 +1,40 @@
 module.exports = exports = function( grunt ) {
     "use strict";
 
-    var _ = grunt.util._;
-
-    function jqueryTestPage( file, version ) {
-        return "http://localhost:<%= connect.dev.options.port %>/tests/unit/" + file + ".html?jquery=" + version;
-    }
-
     grunt.initConfig({
+        // Watch
+        // ------------------------------------------------------------
         watch: {
             less: {
                 files: [ "styles/*.less" ],
-                tasks: [ "default" ]
+                tasks: [ "styles" ]
             },
             js: {
                 files: [ "scripts/*.js" ],
-                tasks: [ "concat" ]
+                tasks: [ "scripts" ]
             }
         },
-        jshint: {
+
+        // Styles
+        // ------------------------------------------------------------
+        less: {
             dev: {
-                options: {
-                    jshintrc: ".jshintrc"
-                },
                 files: {
-                    src: [ "scripts/*.js" ]
+                    "dist/frontkit.css": "styles/frontkit.less"
                 }
             }
         },
-        qunit: {
-            dev: {
-                options: {
-                    urls: _.flatten([ "1.9.0", "1.10.0", "2.0.0" ].map(function( version ) {
-                        return [
-                            jqueryTestPage( "core", version ),
-                            jqueryTestPage( "affix", version )
-                        ];
-                    }))
-                }
-            }
+
+        // Scripts
+        // ------------------------------------------------------------
+        jshint: {
+            options: {
+                jshintrc: ".jshintrc"
+            },
+            all: [ "scripts/*.js" ]
         },
         jscs: {
-            dev: [ "scripts/*.js", "tests/unit/spec/**/*.js" ]
-        },
-        jquery: {
-            dev: {
-                output: "tests/unit/lib/jquery",
-                versions: [ "1.9.0", "1.10.0", "2.0.0" ]
-            }
-        },
-        connect: {
-            dev: {
-                options: {
-                    port: 5050
-                }
-            }
+            all: [ "scripts/*.js" ]
         },
         concat: {
             dev: {
@@ -62,32 +42,26 @@ module.exports = exports = function( grunt ) {
                 dest: "dist/frontkit.js"
             }
         },
-        less: {
-            dev: {
-                files: {
-                    "dist/frontkit.css": "styles/frontkit.less"
-                }
-            }
+        qunit: {
+            dev: [
+                "tests/unit/core.html",
+                "tests/unit/affix.html"
+            ]
         }
     });
 
+    // Load all deps...
     grunt.loadNpmTasks( "grunt-contrib-less" );
     grunt.loadNpmTasks( "grunt-contrib-watch" );
     grunt.loadNpmTasks( "grunt-contrib-concat" );
     grunt.loadNpmTasks( "grunt-contrib-qunit" );
     grunt.loadNpmTasks( "grunt-contrib-jshint" );
-    grunt.loadNpmTasks( "grunt-contrib-connect" );
     grunt.loadNpmTasks( "grunt-jscs-checker" );
-    grunt.loadNpmTasks( "grunt-jquery-builder" );
 
-    grunt.registerTask( "styles", [ "less" ] );
-    grunt.registerTask( "scripts", [
-        "jshint:dev",
-        "jscs:dev",
-        "jquery:dev",
-        "connect:dev",
-        "qunit:dev",
-        "concat:dev"
-    ]);
-    grunt.registerTask( "default", [ "styles", "scripts" ] );
+    // Group tasks by scripts/styles
+    grunt.registerTask( "styles", [ "less" ]);
+    grunt.registerTask( "scripts", [ "jshint", "jscs", "qunit", "concat" ]);
+
+    // Mixed tasks
+    grunt.registerTask( "default", [ "styles", "scripts" ]);
 };
