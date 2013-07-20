c84120f230a1e3a8a7e88e8a9638a604b7ede87a
Gustavo Henke <gustavo@injoin.com.br>
2013-07-20 01:12:52 -0300

    Tests need jQuery 2, too

--- tests/unit/lib/jquery-loader.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    /*jshint evil:true */
-    "use strict";
-
-    var jq = location.search.match( /[?&]jquery=(.*?)(?=&|$)/ );
-    var url = "lib/jquery/" + ( jq ? jq[ 1 ] : "1.10.0" ) + ".js";
-
-    document.write( "<script src='" + url + "'></script>" );
-})();
